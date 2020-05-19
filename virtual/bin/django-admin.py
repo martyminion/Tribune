efff5d50978a89ba7eb459fcf1bc13f9916188e2
martyminion <martkimwaweru@gmail.com>
2020-05-19 21:53:22 +0300

    bg:removed virtual

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/martin/Documents/Moringa-Core/Django/The-Moringa-Tribune/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
