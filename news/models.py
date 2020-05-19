f9a1f679c23ae5eb45a3209f8285ab0666e0b816
martyminion <martkimwaweru@gmail.com>
2020-05-19 21:51:01 +0300

    ft:set up django admin

--- news/models.py
+++ news/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.EmailField()
+  phone_number = models.CharField(max_length=10,blank=True)
+
+  def __str__(self):
+    return self.first_name
+
+  def save_editor(self):
+    self.save()
+
+  def delete_editor(self):
+    self.delete()
+
+  def get_all_editors():
+    all_editors = Editor.objects.all()
+    return all_editors
+
+class tags(models.Model):
+  name = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
+
+  def save_tags(self):
+    self.save()
+
+  def get_all_tags():
+    all_tags = tags.objects.all()
+    return all_tags
+
+class Article(models.Model):
+  title = models.CharField(max_length=60)
+  post = models.TextField()
+  editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+  tags = models.ManyToManyField(tags)
+  pub_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
+
+  def save_article(self):
+    self.save()
+
+  @classmethod
+  def delete_article(cls,id):
+    Article.objects.filter(id = id).delete()
+
+  def get_all_articles():
+    all_articles = Article.objects.all()
+    return all_articles
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
-
+from .models import Editor,tags,Article
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+  #Set up Method
+
+  def setUp(self):
+    self.james = Editor(first_name = "James", last_name = "Muriuki", email = "james@gmail.com")
+    self.marge = Editor(first_name = "Margaret", last_name = "Wamucii", email = "margie@gmail.com")
+
+  #test an instance of the class
+  def test_instance(self):
+    self.assertTrue(isinstance(self.james,Editor))
+
+  def test_save_method(self):
+    self.james.save_editor()
+    editors = Editor.objects.all()
+    self.assertTrue(len(editors)>0)
+
+  def test_delete_editor(self):
+    self.james.save_editor()
+    self.james.delete_editor()
+    editors = Editor.objects.all()
+    self.assertTrue(len(editors) == 0)
+
+  def test_get_all_editors(self):
+    self.james.save_editor()
+    self.marge.save_editor()
+    editors = Editor.get_all_editors()
+    self.assertTrue(len(editors) == 2)
