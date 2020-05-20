54eb754aa34b67ccc992f451496bf286e47e0eab
martyminion <martkimwaweru@gmail.com>
2020-05-20 12:19:53 +0300

    ft:upload image functionality

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 #note the syntax for 3.0.6
@@ -8,3 +10,5 @@ urlpatterns = [
   path('archives/<past_date>',views.past_days_news, name = 'pastNews'),
   path('search/',views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+  urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tribune/settings.py
+++ tribune/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,3 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  
 ]
+
+#how to handle media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#this allows acces to uploaded images through localhost:8000/media/articles/image.jpg 
