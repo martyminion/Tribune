bcee072b7c19a931dce26bd5212748b06c617b0d
martyminion <martkimwaweru@gmail.com>
2020-05-30 18:56:03 +0300

    initial pish

--- tribune/urls.py
+++ tribune/urls.py
@@ -25,8 +25,10 @@ urlpatterns = [
     path('',include('news.urls')),
     path('accounts/register/',RegistrationView.as_view(success_url='/'),name='django_registration_register'),
     path('accounts/',include('django_registration.backends.one_step.urls')),
+    
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/login', LoginView.as_view(redirect_field_name = '/',success_url = '/'),name='login'),
+    
     path('tinymce/',include('tinymce.urls')),
     
 ]
